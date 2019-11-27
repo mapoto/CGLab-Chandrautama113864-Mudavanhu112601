f11abb88df10ff8f67f114f965147be2bb029b6e
philbrading <philbranding@gmail.com>
2019-11-27 15:28:23 +0100

    "Commented the Node Class and the Hpp"

--- framework/include/Node.hpp
+++ framework/include/Node.hpp
@@ -9,6 +9,13 @@
 #include <list>
 #include <memory>
 
+/*
+The Node Class is responsible for creating the different Nodes in the Sceen Graph.
+Some A Root Node is the Ancestor node for the rest of all the nodes that that are in the tree
+Each created Node can either be a Parent or Child Node. Each created node is given a name to be 
+used to identify it when parsing through the tree. 
+*/
+
 class Node {
  protected:
   Node* parent_;
@@ -20,27 +27,40 @@ class Node {
   glm::mat4 worldTransform_;
 
  public:
+  //User Defined Constructor
   Node(std::string const& name);
+
+  //Default Constructor
   Node();
+
+  //Destructor 
   ~Node();
 
+  //Used to Set a Node as a Parent Node
   Node* getParent() const;
   void setParent(Node* parent);
 
+  //Used to created a List of all the Children Nodes in a Tree (Scene Graph)
   Node* getChild(std::string const& name) const;
   std::list<Node*> getChildrenList() const;
 
+  // Used to identify the Name of the Child and get the path to the Node and see how dep it is in the Tree
   std::string getName() const;
   std::string getPath() const;
   int getDepth() const;
 
+  //Used to set the initial Local Transformation Matrix of the Created Node/ Child/ Parent
   glm::mat4 getLocalTransform() const;
   void setLocalTransform(glm::mat4 const& inputMatrix);
 
+  //Used to set the initial World Transformation Matrix of the Created Node/ Child/ Parent
   glm::mat4 getWorldTransform() const;
   void setWorldTransform(glm::mat4 const& inputMatrix);
 
+  //Used to add or Creat a new Node/ Child/ Parent
   void addChild(Node* node);
+
+  //Used to remove a Node/ Child/ Parent by specifiying the name of the Node 
   Node* removeChild(std::string const& name);
 };
 
--- framework/source/Node.cpp
+++ framework/source/Node.cpp
@@ -1,5 +1,8 @@
 #include "Node.hpp"
 
+////////////////////////////////////////////////////////////////////////////////
+
+//User defined Constructor Used to create a new Node and assign a name, a path and the depth in the Tree(Scenegraph)
 Node::Node(std::string const& name)
     : name_{name}, path_{"\\" + name_}, depth_{0} {
   localTransform_ = glm::fmat4{};
@@ -7,11 +10,16 @@ Node::Node(std::string const& name)
   parent_ = nullptr;
 }
 
+////////////////////////////////////////////////////////////////////////////////
+
+//Default Constructor Used to create a new Node and assign a defualt name, a path and a defualt depth in the Tree(Scenegraph)
 Node::Node() : name_{"name"}, path_{"\\" + name_}, depth_{0} {
   localTransform_ = glm::fmat4{};
   worldTransform_ = glm::fmat4{};
   parent_ = nullptr;
 }
+
+////////////////////////////////////////////////////////////////////////////////
 
 Node::~Node() {
   // clear connection between this node and its parent if it has one
@@ -34,18 +42,21 @@ Node::~Node() {
 }
 
 ////////////////////////////////////////////////////////////////////////////////
+//Used to access the Parent Node of the Child Node in a Tree 
 
 Node* Node::getParent() const {
   return parent_;
 }
 
 ////////////////////////////////////////////////////////////////////////////////
+//Used to set a  Parent Node for a given Child Node in a Tree 
 
 void Node::setParent(Node* parent) {
   parent_->addChild(this);
 }
 
 ////////////////////////////////////////////////////////////////////////////////
+//Used to to parse Node in a Tree and get a specific Node from the List 
 
 Node* Node::getChild(std::string const& name) const {
   if (!children_.empty()) {
@@ -61,47 +72,55 @@ Node* Node::getChild(std::string const& name) const {
 }
 
 ////////////////////////////////////////////////////////////////////////////////
+//Used to create a List of all the Children Nodes in a Tree
 
 std::list<Node*> Node::getChildrenList() const {
   return children_;
 }
 
 ////////////////////////////////////////////////////////////////////////////////
+//Function for getting the name of the Node
 
 std::string Node::getName() const {
   return name_;
 }
+
+//Function for getting the Path to the Node
 
 std::string Node::getPath() const {
   return path_;
 }
 
 ////////////////////////////////////////////////////////////////////////////////
+//Function used to get the Depth of a Node in the Tree 
 
 int Node::getDepth() const {
   return depth_;
 }
 
 ////////////////////////////////////////////////////////////////////////////////
+//Function for the getting the Local Transformation matrix of the created Node Planet)
 
 glm::mat4 Node::getLocalTransform() const {
   return localTransform_;
 }
 
 ////////////////////////////////////////////////////////////////////////////////
+//Function for the setting the Local Transformation matrix of a new Node (Planet)
 
 void Node::setLocalTransform(glm::mat4 const& inputMatrix) {
   localTransform_ = inputMatrix;
 }
 
 ////////////////////////////////////////////////////////////////////////////////
+// For getting the World Transformation of a Node or Planet
 
 glm::mat4 Node::getWorldTransform() const {
   return worldTransform_;
 }
 
 ////////////////////////////////////////////////////////////////////////////////
-
+//
 void Node::setWorldTransform(glm::mat4 const& inputMatrix) {
   worldTransform_ = inputMatrix;
 }
